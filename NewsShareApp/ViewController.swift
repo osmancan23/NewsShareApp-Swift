52bb053983cf92c4f8adaeb0782dda4fede46611
osmancan23 <osmacan2319072@gmail.com>
2024-01-31 20:05:56 +0300

    create project

---
+++ NewsShareApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsShareApp
+//
+//  Created by Osmancan Akagündüz on 31.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
