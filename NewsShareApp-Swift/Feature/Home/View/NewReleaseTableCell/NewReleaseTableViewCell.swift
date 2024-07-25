1cc600f5afe34e23137f44300108a70491b8bd9c
Osmancan Akagündüz <83430594+osmancan23@users.noreply.github.com>
2024-07-25 16:07:36 +0300

    completed home view

---
+++ NewsShareApp-Swift/Feature/Home/View/NewReleaseTableCell/NewReleaseTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewReleaseTableViewCell.swift
+//  NewsShareApp-Swift
+//
+//  Created by Osmancan Akagündüz on 25.07.2024.
+//
+
+import UIKit
+
+class NewReleaseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ NewsShareApp-Swift/Feature/Home/View/PopularNewsCollectionCell/PopularNewsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PopularNewsCollectionViewCell.swift
+//  NewsShareApp-Swift
+//
+//  Created by Osmancan Akagündüz on 24.07.2024.
+//
+
+import UIKit
+
+class PopularNewsCollectionViewCell: UICollectionViewCell {
+    
+}
